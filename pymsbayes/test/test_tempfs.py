d8f8772d1e7ddc0f7fa262fc77b2be1f8ba05441
Jamie Oaks <joaks1@gmail.com>
2013-01-16 01:44:26 -0600

    Adding preliminary tests of TempFileSystem.
    Adding test_tempfs.py to test suite.

---
+++ pymsbayes/test/test_tempfs.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import unittest
+import os
+
+from pymsbayes.utils import tempfs
+from pymsbayes.test.support import package_paths
+from pymsbayes.utils.messaging import get_logger
+
+_LOG = get_logger(__name__)
+
+class TempFileSystemTestCase(unittest.TestCase):
+    def setUp(self):
+        self.parent = package_paths.output_path()
+        os.mkdir(self.parent)
+        self.prefix = 'test-temp'
+
+    def tearDown(self):
+        os.rmdir(self.parent)
+
+    def test_initiate_purge(self):
+        tmp = tempfs.TempFileSystem(parent=self.parent, prefix=self.prefix)
+        self.assertIsInstance(tmp, tempfs.TempFileSystem)
+        self.assertTrue(os.path.exists(tmp.base_dir))
+        self.assertTrue(os.path.isdir(tmp.base_dir))
+        self.assertEqual(tmp.prefix, self.prefix)
+        self.assertEqual(tmp.parent, self.parent.rstrip(os.path.sep))
+        self.assertTrue(os.path.basename(tmp.base_dir).startswith(
+                self.prefix))
+        self.assertFalse(tmp.deleted)
+        tmp.purge()
+        self.assertTrue(tmp.deleted)
+
+if __name__ == '__main__':
+    unittest.main()
+
