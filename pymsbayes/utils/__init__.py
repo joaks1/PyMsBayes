23c1ab47dcb961914a699b6a5458b0b7c36bd750
Jamie Oaks <joaks1@gmail.com>
2013-03-18 16:27:47 -0500

    Committing utils.__init__ without functions.
    As per the last commit util functions have moved to separate
    file.

--- pymsbayes/utils/__init__.py
+++ pymsbayes/utils/__init__.py
@@ -2,10 +2,8 @@
 
 import sys
 import os
-import errno
-import random
 import platform
-import string
+import random
 
 GLOBAL_RNG = random.Random()
 PLATFORM = platform.system().lower()
@@ -20,26 +18,4 @@ elif PLATFORM == 'darwin':
     BIN_DIR = os.path.join(base_dir, "bin", "mac", "bin")
 elif PLATFORM == 'windows':
     BIN_DIR = os.path.join(base_dir, "bin", "win", "bin")
-
-def mkdr(path):
-    """
-    Creates directory `path`, but suppresses error if `path` already exists.
-    """
-    try:
-        os.makedirs(path)
-    except OSError, e:
-        if e.errno == errno.EEXIST:
-            pass
-        else:
-            raise
-
-def random_str(length=8,
-        char_pool=string.ascii_letters + string.digits):
-    return ''.join(random.choice(char_pool) for i in range(length))
-
-def get_random_int():
-    return GLOBAL_RNG.randint(1, 999999999)
-
-def expand_path(path):
-    return os.path.abspath(os.path.expandvars(os.path.expanduser(path)))
 
