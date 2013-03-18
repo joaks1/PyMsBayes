0f537dc7f3d19b23266298d693c110aec051c5f8
Jamie Oaks <joaks1@gmail.com>
2013-03-18 16:26:18 -0500

    Moving utils functions to separate file.
    Moving utility functions from pymsbayes.utils.__init__ to
    pymsbayes.utils.functions. Also, adding tests for some of these
    functions.

---
+++ pymsbayes/test/test_util_functions.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+import unittest
+import os
+
+from pymsbayes.utils import functions
+from pymsbayes.test.support import package_paths
+from pymsbayes.test.support.pymsbayes_test_case import PyMsBayesTestCase
+
+class MkdrTestCase(PyMsBayesTestCase):
+    def setUp(self):
+        self.set_up()
+        self.path = os.path.join(self.tempf.base_dir, 'mkdr', 'test', 'dir')
+
+    def tearDown(self):
+        self.tear_down()
+    
+    def test_mkdr(self):
+        self.assertFalse(os.path.exists(self.path))
+        functions.mkdr(self.path)
+        self.assertTrue(os.path.exists(self.path))
+        self.assertTrue(os.path.isdir(self.path))
+        functions.mkdr(self.path)
+        self.assertTrue(os.path.exists(self.path))
+        self.assertTrue(os.path.isdir(self.path))
+
+class WhereisTestCase(unittest.TestCase):
+    def test_whereis(self):
+        path = functions.whereis('more')
+        self.assertIsNotNone(path)
+        self.assertTrue(os.path.exists(path))
+        path = functions.whereis('x_bogus_exe_name_x')
+        self.assertIsNone(path)
+
+class IsFileTestCase(unittest.TestCase):
+    def setUp(self):
+        self.file = package_paths.data_path("msbayes_sim.xml")
+        self.bogus_file = package_paths.data_path("bogusdatafilename")
+    
+    def test_is_file(self):
+        self.assertFalse(functions.is_file(None))
+        self.assertFalse(functions.is_file(self.bogus_file))
+        self.assertTrue(functions.is_file(self.file))
+        
+class IsExecutableTestCase(unittest.TestCase):
+    def setUp(self):
+        self.file = package_paths.data_path("msbayes_sim.xml")
+        self.bogus_file = package_paths.data_path("bogusdatafilename")
+        self.exe = package_paths.bin_path('msbayes_mod.pl')
+    
+    def test_is_executable(self):
+        self.assertFalse(functions.is_executable(None))
+        self.assertFalse(functions.is_executable(self.bogus_file))
+        self.assertFalse(functions.is_executable(self.file))
+        self.assertTrue(functions.is_executable(self.exe))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ pymsbayes/utils/functions.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import errno
+import random
+import string
+
+from pymsbayes.utils import GLOBAL_RNG
+
+def mkdr(path):
+    """
+    Creates directory `path`, but suppresses error if `path` already exists.
+    """
+    try:
+        os.makedirs(path)
+    except OSError, e:
+        if e.errno == errno.EEXIST:
+            pass
+        else:
+            raise
+
+def random_str(length=8,
+        char_pool=string.ascii_letters + string.digits):
+    return ''.join(random.choice(char_pool) for i in range(length))
+
+def get_random_int():
+    return GLOBAL_RNG.randint(1, 999999999)
+
+def expand_path(path):
+    return os.path.abspath(os.path.expandvars(os.path.expanduser(path)))
+
+def get_indices_of_patterns(target_list, regex_list):
+    indices = []
+    for regex in regex_list:
+        indices.extend([i for i, e in enumerate(target_list) if regex.match(e)])
+    return sorted(indices)
+
+def get_indices_of_strings(target_list, string_list):
+    indices = []
+    for s in string_list:
+        indices.extend([i for i, e in enumerate(target_list) if s.strip() == e.strip()])
+    return sorted(indices)
+    
+def reduce_columns(in_stream, out_stream, column_indices, sep='\t',
+        extra_tab=False):
+    line_iter = iter(in_stream)
+    for line_num, line in enumerate(line_iter):
+        l = line.strip().split()
+        new_line = [l[i] for i in column_indices]
+        if extra_tab:
+            out_stream.write('%s\t\n' % sep.join(new_line))
+        else:
+            out_stream.write('%s\n' % sep.join(new_line))
+
+def list_splitter(l, n, by_size=False):
+    """
+    Returns generator that yields list `l` as `n` sublists, or as `n`-sized
+    sublists if `by_size` is True.
+    """
+    if by_size:
+        for i in range(0, len(l), n):
+            yield l[i:i+n]
+    else:
+        for i in range(0, len(l), (len(l)/int(n))):
+            yield l[i:i+(len(l)/int(n))]
+
+def whereis(file_name):
+    """
+    Returns the first absolute path to `file_name` encountered in $PATH.
+    Returns `None` if `file_name` is not found in $PATH.
+    """
+    paths = os.environ.get('PATH', '').split(':')
+    for path in paths:
+        abs_path = os.path.join(path, file_name)
+        if os.path.exists(abs_path) and not os.path.isdir(abs_path):
+            return abs_path
+            break
+    return None
+
+def is_file(path):
+    if not path:
+        return False
+    if not os.path.isfile(path):
+        return False
+    return True
+
+def is_executable(path):
+    is_f = is_file(path)
+    if not is_f:
+        return False
+    if (os.stat(path)[stat.ST_MODE] & (stat.S_IXUSR|stat.S_IXGRP|stat.S_IXOTH)) == 0:
+        return False
+    return True
+
