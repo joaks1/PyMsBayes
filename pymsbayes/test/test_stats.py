4311fd3f449ec56f7349e2d4a65373e191f3fe78
Jamie Oaks <joaks1@gmail.com>
2013-03-20 21:41:48 -0500

    Adding stats module and associated tests.

---
+++ pymsbayes/test/test_stats.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+import unittest
+import os
+import math
+
+from pymsbayes.utils.stats import SampleSummarizer
+from pymsbayes.test.support.pymsbayes_test_case import PyMsBayesTestCase
+from pymsbayes.utils.messaging import get_logger
+
+_LOG = get_logger(__name__)
+
+class SampleSummarizerTestCase(PyMsBayesTestCase):
+
+    def test_init(self):
+        ss = SampleSummarizer('test')
+        self.assertEqual(ss.name, 'test')
+        self.assertEqual(ss.minimum, None)
+        self.assertEqual(ss.maximum, None)
+        self.assertEqual(ss.mean, None)
+        self.assertEqual(ss.variance, None)
+        self.assertEqual(ss.std_deviation, None)
+        self.assertEqual(ss.pop_variance, None)
+
+    def test_add_one_sample(self):
+        ss = SampleSummarizer('test')
+        ss.add_sample(1)
+        self.assertEqual(ss.name, 'test')
+        self.assertEqual(ss.minimum, 1)
+        self.assertEqual(ss.maximum, 1)
+        self.assertApproxEqual(ss.mean, 1.0, 1e-9)
+        self.assertEqual(ss.variance, float('inf'))
+        self.assertEqual(ss.std_deviation, float('inf'))
+        self.assertEqual(ss.pop_variance, 0)
+
+        ss = SampleSummarizer('test')
+        ss.add_sample(3.45)
+        self.assertEqual(ss.name, 'test')
+        self.assertEqual(ss.minimum, 3.45)
+        self.assertEqual(ss.maximum, 3.45)
+        self.assertApproxEqual(ss.mean, 3.45, 1e-9)
+        self.assertEqual(ss.variance, float('inf'))
+        self.assertEqual(ss.std_deviation, float('inf'))
+        self.assertEqual(ss.pop_variance, 0)
+
+    def test_update_samples(self):
+        ss = SampleSummarizer('test')
+        ss.update_samples([1.0, 2.0, 3.0])
+        self.assertEqual(ss.name, 'test')
+        self.assertEqual(ss.minimum, 1.0)
+        self.assertEqual(ss.maximum, 3.0)
+        self.assertApproxEqual(ss.mean, 2.0, 1e-9)
+        self.assertApproxEqual(ss.variance, 1.0, 1e-9)
+        self.assertEqual(ss.std_deviation, math.sqrt(1.0), 1e-9)
+        self.assertApproxEqual(ss.pop_variance, 2/float(3), 1e-9)
+
+if __name__ == '__main__':
+    unittest.main()
+
---
+++ pymsbayes/utils/stats.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import math
+from cStringIO import StringIO
+
+from pymsbayes.utils.messaging import get_logger
+
+_LOG = get_logger(__name__)
+
+class SampleSummarizer(object):
+    def __init__(self, name='sample summarizer'):
+        self.name = name
+        self._min = None
+        self._max = None
+        self._n = 0
+        self._sum = 0
+        self._sum_of_squares = 0
+    
+    def add_sample(self, x):
+        self._n += 1
+        self._sum += x
+        self._sum_of_squares += x*x
+        if not self._min:
+            self._min = x
+        elif x < self._min:
+            self._min = x
+        if not self._max:
+            self._max = x
+        elif x > self._max:
+            self._max = x
+
+    def update_samples(self, x_iter):
+        for x in x_iter:
+            self.add_sample(x)
+
+    def _get_min(self):
+        return self._min
+
+    def _get_max(self):
+        return self._max
+
+    minimum = property(_get_min)
+    maximum = property(_get_max)
+    
+    def _get_mean(self):
+        if self._n < 1:
+            return None
+        return float(self._sum)/self._n
+    
+    def _get_variance(self):
+        if self._n < 1:
+            return None
+        if self._n == 1:
+            return float('inf')
+        return (self._sum_of_squares - (self._get_mean()*self._sum))/(self._n-1)
+
+    def _get_std_dev(self):
+        if self._n < 1:
+            return None
+        return math.sqrt(self._get_variance())
+
+    def _get_pop_variance(self):
+        if self._n < 1:
+            return None
+        return (self._sum_of_squares - (self._get_mean()*self._sum))/self._n
+
+    mean = property(_get_mean)
+    variance = property(_get_variance)
+    pop_variance = property(_get_pop_variance)
+    std_deviation = property(_get_std_dev)
+
+    def __str__(self):
+        s = StringIO()
+        s.write('sample size = {0}\n'.format(self._n))
+        s.write('sum = {0}\n'.format(self._sum))
+        s.write('sum of squares = {0}\n'.format(self._sum_of_squares))
+        s.write('min = {0}\nmax = {1}\n'.format(self._min, self._max))
+        s.write('mean = {0}\n'.format(self.mean))
+        s.write('variance = {0}\n'.format(self.variance))
+        s.write('pop variance = {0}\n'.format(self.pop_variance))
+        return s.getvalue()
+
+
