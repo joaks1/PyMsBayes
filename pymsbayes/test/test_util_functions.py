d1f65647b1c7e105b8aa5786ec88c538d5191800
Jamie Oaks <joaks1@gmail.com>
2013-03-29 17:27:45 -0500

    Adding tests for `is_dir` and `long_division` functions

--- pymsbayes/test/test_util_functions.py
+++ pymsbayes/test/test_util_functions.py
@@ -47,6 +47,16 @@ class IsFileTestCase(unittest.TestCase):
         self.assertFalse(functions.is_file(self.bogus_file))
         self.assertTrue(functions.is_file(self.file))
         
+class IsdirTestCase(unittest.TestCase):
+    def setUp(self):
+        self.dir = package_paths.data_path()
+        self.file = package_paths.data_path("4pairs_1locus.cfg")
+    
+    def test_is_dir(self):
+        self.assertFalse(functions.is_dir(None))
+        self.assertFalse(functions.is_dir(self.file))
+        self.assertTrue(functions.is_dir(self.dir))
+
 class IsExecutableTestCase(unittest.TestCase):
     def setUp(self):
         self.file = package_paths.data_path("4pairs_1locus.cfg")
@@ -58,6 +68,12 @@ class IsExecutableTestCase(unittest.TestCase):
         self.assertFalse(functions.is_executable(self.bogus_file))
         self.assertFalse(functions.is_executable(self.file))
         self.assertTrue(functions.is_executable(self.exe))
+
+class LongDivisionTestCase(unittest.TestCase):
+    def test_long_division(self):
+        self.assertEqual(functions.long_division(5, 2), (2, 1))
+        self.assertEqual(functions.long_division(6, 2), (3, 0))
+        self.assertEqual(functions.long_division(-11, 3), (-4, 1))
 
 if __name__ == '__main__':
     unittest.main()
