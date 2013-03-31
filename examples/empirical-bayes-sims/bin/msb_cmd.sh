49ff0f090155e62c6224594d55ce787fe34ac1bd
Jamie Oaks <joaks1@gmail.com>
2013-03-30 22:10:53 -0500

    Adding SGE qsub options to `msb_cmd.sh`.
    Adding the SGE syntax to the bash script for launching `msp.py`
    within the empirical bayes example.

--- examples/empirical-bayes-sims/bin/msb_cmd.sh
+++ examples/empirical-bayes-sims/bin/msb_cmd.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
+#! /bin/sh
+#$ -S /bin/bash
+#$ -cwd
+#$ -V
+#$ -l h_vmem=2G
+#$ -l vf=2G
+#$ -q all.q
+#$ -pe orte 10
+
+if [ -n "$SGE_O_WORKDIR" ]
+then
+    source ~/.bash_profile
+    cd /share/work1
+    cd $SGE_O_WORKDIR
+fi
 
 nprocs = 10
 nreps = 100
