4135896db7656ab41b0bfb1fac7d0480ec451c06
Jamie Oaks <joaks1@gmail.com>
2013-04-01 16:21:20 -0500

    Updating qsub script for example.
    Adding invocation via mpirun to see if this achieves better MPI
    behavior on Rocks cluster.

--- examples/empirical-bayes-sims/bin/msb_cmd.sh
+++ examples/empirical-bayes-sims/bin/msb_cmd.sh
@@ -19,7 +19,12 @@ nreps=100
 nprior=10000
 npost=100
 
-msb.py --np $nprocs -o ../configs/m5.cfg -p ../configs/*.cfg -r $nreps \
-    -n $nprior --num-posterior-samples $npost --output-dir ../results \
+mpirun -np $nprocs msb.py --np $nprocs \
+    -o ../configs/m5.cfg \
+    -p ../configs/*.cfg \
+    -r $nreps \
+    -n $nprior \
+    --num-posterior-samples $npost \
+    --output-dir ../results \
     --merge-priors --keep-priors --seed 4849390
 
