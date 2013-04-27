79cabca4bb16731cb32ac6999ee5839e229f2ea9
Jamie Oaks <joaks1@gmail.com>
2013-04-27 10:21:30 -0500

    More work on `teams` module.
    Still in prep.

--- pymsbayes/teams.py
+++ pymsbayes/teams.py
@@ -12,184 +12,150 @@ from pymsbayes.utils.messaging import get_logger
 process_file_arg
 parse_header
 get_stat_indices
+Manager
+list_splitter
 
 _LOG = get_logger(__name__)
 
-def assemble_abc_teams:
-        temp_fs,
-        observed_sims_file,
-        prior_config_dict, # model index by cfg path dict
-        num_prior_samples,
-        num_processors,
-        num_posterior_samples = 1000,
-        batch_size = 10000,
-        rng = None,
-        sort_index = None,
-        report_parameters = True,
-        stat_patterns = DEFAULT_STAT_PATTERNS,
-        parameter_patterns = PARAMETER_PATTERNS,
-        msbayes_exe_path = None,
-        abctoolbox_exe_path = None,
-        keep_temps = False):
-    if not rng:
-        rng = GLOBAL_RNG
-    if batch_size < num_posterior_samples:
-        batch_size = num_posterior_samples
-    obs_file, close = process_file_arg(observed_sims_file)
-    header = parse_header(obs_file)
-    all_stat_indices = get_stat_indices(header,
-            stat_patterns=ALL_STAT_PATTERNS)
-    obs_temp_dir = temp_fs.create_subdir(prefix = 'observed-files-')
-    obs_file.next() # header line
-    observed_paths = []
-    for i, line in enumerate(obs_file):
-        rej_obs_path = temp_fs.get_file_path(parent = obs_temp_dir,
-                prefix = 'observed-{0}-'.format(i+1),
-                create = False)
-        l = line.strip().split()
-        with open(rej_obs_path, 'w') as out:
-            out.write('{0}\n{1}\n'.format(
-                    '\t'.join([header[idx] for idx in all_stat_indices]),
-                    '\t'.join([str(l[idx]) for idx in all_stat_indices])))
-        observed_paths.append(rej_obs_path)
-    if close:
-        obs_file.close()
-    num_teams = least_common_multiple([num_processors, len(prior_config_dict)])
-    num_teams_per_config, r = long_division(num_teams, len(prior_config_dict))
-    assert r == 0
-    num_samples_per_team, extra_samples = long_division(num_prior_samples,
-            num_teams_per_config)
-    num_batches_per_team, r = long_division(num_samples_per_team, batch_size)
-    all_teams = []
-    batch_idx = 0
-    for model_idx, cfg_path in prior_config_dict.iteritems():
-        model_teams = []
-        for num_teams_per_config:
-            seeds = [get_random_int(rng) for i in range(len(
-                    num_batches_per_team))]
-            model_teams.append(ABCTeam(
-                    temp_fs = temp_fs,
-                    observed_paths = observed_paths,
-                    prior_config_path = cfg_path,
-                    num_prior_samples = num_samples_per_team,
-                    seeds = seeds,
-                    num_posterior_samples = num_posterior_samples,
-                    model_index = model_idx,
-                    sort_index = sort_index,
-                    report_parameters = report_parameters,
-                    msbayes_exe_path = msbayes_exe_path,
-                    abctoolbox_exe_path = abctoolbox_exe_path,
-                    keep_temps = keep_temps))
-        model_teams[-1].num_prior_samples += extra_samples
-        all_teams.extend(model_teams)
-    return all_teams
-
-def assemble_final_rejection_workers_from_abc_teams(
-        abc_teams,
-        regress = True,
-        regression_method = 'glm'):
-    # create rejection workers for final reject/regression 
-    # sort rejection_workers of each team by tag and check the obs_paths
-    # match during merge
-    pass
-
 class ABCTeam(object):
+    count = 0
     def __init__(self,
             temp_fs,
-            observed_paths,
-            prior_config_path,
+            observed_sims_file,
+            model_indices_to_config_paths,
             num_prior_samples,
-            seeds,
+            num_processors,
             num_posterior_samples = 1000,
-            model_index = None,
+            batch_size = 10000,
+            rng = None,
             sort_index = None,
             report_parameters = True,
             stat_patterns = DEFAULT_STAT_PATTERNS,
             parameter_patterns = PARAMETER_PATTERNS,
             msbayes_exe_path = None,
             abctoolbox_exe_path = None,
-            keep_temps = False):
+            keep_temps = False,
+            work_queue = WORK_FORCE):
+        if not rng:
+            rng = GLOBAL_RNG
+        self.rng = rng
+        self.__class__.count += 1
+        self.name = 'RejectionTeam-' + str(self.count)
         self.temp_fs = temp_fs
-        self.observed_paths = observed_paths
-        self.config_path = prior_config_path
+        self.observed_temp_dir = temp_fs.create_subdir(
+                prefix = 'observed-files-')
+        self.observed_sims_path = observed_sims_path
+        self.models = model_indices_to_config_paths
         self.num_prior_samples = num_prior_samples
+        self.num_processors = num_processors
         self.num_posterior_samples = num_posterior_samples
-        self.model_index = model_index
+        self.batch_size = batch_size
+        self.total_samples = self.num_prior_samples * len(self.models)
+        max_samples_per_process = self.total_samples / self.num_processors
+        if max_samples_per_process < self.batch_size:
+            self.batch_size = max_samples_per_process
+        if self.batch_size < self.num_posterior_samples:
+            self.batch_size = self.num_posterior_samples
+        self.num_batches, self.num_extra_samples = long_division(
+                self.num_prior_samples, 
+                self.batch_size)
         self.sort_index = sort_index
         self.report_parameters = report_parameters
         self.msbayes_exe_path = msbayes_exe_path
         self.abctoolbox_exe_path = abctoolbox_exe_path
-        batch_size, remainder = long_division(self.num_prior_samples,
-                len(seeds))
-        batch_sizes = [batch_sizes] * len(seeds)
-        if remainder > 0:
-            batch_sizes[-1] += remainder
-        self.seeds = dict((seeds[i], batch_sizes[i]) for i in range(len(seeds)))
+        self.rejection_teams = []
         self.prior_workers = []
-        self.rejection_workers = []
         self.keep_temps = keep_temps
+        self.work_queue = work_queue
+        self.result_queue = multiprocessing.Queue()
 
-    def _pre_process(self):
-        for seed, nsamples in self.seeds:
-            self.prior_workers.append(MsBayesWorker(
+    def _assemble_rejection_teams(self):
+        obs_file, close = process_file_arg(self.observed_sims_path)
+        header = parse_header(obs_file)
+        all_stat_indices = get_stat_indices(header,
+                stat_patterns=ALL_STAT_PATTERNS)
+        obs_file.next() # header line
+        for i, line in enumerate(obs_file):
+            obs_path = temp_fs.get_file_path(parent = self.observed_temp_dir,
+                    prefix = 'observed-{0}-'.format(i+1),
+                    create = False)
+            l = line.strip().split()
+            with open(obs_path, 'w') as out:
+                out.write('{0}\n{1}\n'.format(
+                        '\t'.join([header[idx] for idx in all_stat_indices]),
+                        '\t'.join([str(l[idx]) for idx in all_stat_indices])))
+            self.rejection_teams.append(RejectionTeam(
                     temp_fs = self.temp_fs,
-                    self.sample_size = nsamples,
-                    config_path = self.config_path,
-                    exe_path = self.msbayes_exe_path,
-                    model_index = self.model_index,
-                    sort_index = self.sort_index,
-                    report_parameters = self.report_parameters,
-                    seed = seed,
-                    schema = 'abctoolbox',
-                    stat_patterns = self.stat_patterns,
-                    parameter_patterns = self.parameter_patterns,
-                    write_stats_file = False))
-        for i, obs_path in enumerate(self.observed_paths):
-            self.rejection_workers.append(ABCToolBoxRejectWorker(
-                    temp_fs = temp_fs,
+                    prior_workers = [],
                     observed_path = obs_path,
-                    prior_path = self.prior_workers[0].prior_path,
                     num_posterior_samples = self.num_posterior_samples,
-                    posterior_path = None,
-                    regression_worker = None,
-                    exe_path = self.abctoolbox_exe_path,
-                    keep_temps = self.keep_temps,
-                    max_read_sims = max(self.seeds.values()) + 10,
-                    tag = str(i)))
+                    abctoolbox_exe_path = self.abctoolbox_exe_path,
+                    keep_temps = self.keep_temps))
+        if close:
+            obs_file.close()
+    
+    def _assemble_prior_workers(self):
+        prior_workers = []
+        for model_index, config_path in self.models:
+            for i in range(self.num_batches):
+                seed = get_random_int(self.rng)
+                worker = MsBayesWorker(
+                        temp_fs = self.temp_fs,
+                        sample_size = self.batch_size,
+                        config_path = config_path,
+                        model_index = model_index,
+                        report_parameters = self.report_parameters,
+                        seed = seed,
+                        schema = 'abctoolbox',
+                        stat_patterns = self.stat_patterns)
+                prior_workers.append(worker)
+            if self.extra_samples > 0:
+                worker = MsBayesWorker(
+                        temp_fs = self.temp_fs,
+                        sample_size = self.extra_samples,
+                        config_path = config_path,
+                        model_index = model_index,
+                        report_parameters = self.report_parameters,
+                        seed = seed,
+                        schema = 'abctoolbox',
+                        stat_patterns = self.stat_patterns)
+                prior_workers.append(worker)
+        self.prior_workers = list(list_splitter(prior_workers,
+                self.num_processors))
 
-    def start(self):
+    def run(self):
         self._pre_process()
-        for i in range(len(self.prior_workers)):
-            pw = self.prior_workers[i]
-            pw.start()
-            for j in range(len(self.rejection_workers)):
-                rw = self.rejection_workers[j]
-                if rw.finished:
-                    rw.finished = False
-                    new_prior_path = self.temp_fs.get_file_path(
-                            prefix = 'prior-{0}-{1}-{2}-'.format(rw.name,
-                                    rw.tag, i),
-                            create = False)
-                    new_posterior_path = self.temp_fs.get_file_path(
-                            prefix = 'posterior-{0}-{1}-{2}-'.format(rw.name,
-                                    rw.tag, i),
-                            create = False)
-                    merge_prior_files(
-                            paths = [pw.prior_path, rw.posterior_path],
-                            dest_path = new_prior_path)
-                    if not self.keep_temps:
-                        os.remove(rw.prior_path)
-                        os.remove(rw.posterior_path)
-                    rw.prior_path = new_prior_path
-                    rw.posterior_path = new_posterior_path
-                    rw.start()
-                else:
-                    rw.prior_path = pw.prior_path
-                    rw.posterior_path = self.temp_fs.get_file_path(
-                            prefix = 'posterior-{0}-{1}-{2}-'.format(rw.name,
-                                    rw.tag, i),
-                            create = False)
-                    rw.start()
+        for prior_worker_batch in self.prior_workers:
+            for pw in prior_worker_batch:
+                self.work_queue.put(pw)
+            managers = []
+            for i in range(self.num_processors):
+                m = Manager(work_queue = self.work_queue,
+                        result_queue = self.result_queue)
+                m.start()
+                managers.append(m)
+            for i in range(len(prior_worker_batch)):
+                prior_worker_batch[i] = self.result_queue.get()
+            for m in managers:
+                m.join()
+            assert self.work_queue.empty()
+            assert self.result_queue.empty()
+            for rt in self.rejection_teams:
+                rt.prior_workers = prior_worker_batch
+                self.work_queue.put(rt)
+            managers = []
+            for i in range(self.num_processors):
+                m = Manager(work_queue = self.work_queue,
+                        result_queue = self.result_queue)
+                m.start()
+                managers.append(m)
+            for i in range(len(self.rejection_teams)):
+                self.rejection_teams[i] = self.result_queue.get()
+            for m in managers:
+                m.join()
+            assert self.work_queue.empty()
+            assert self.result_queue.empty()
+
 
 class RejectionTeam(object):
     count = 0
