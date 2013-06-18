ffdd9b1f93feeedd3bdbd82489605f3ee74ed62f
Jamie Oaks <joaks1@gmail.com>
2013-06-18 17:48:21 -0500

    Adding support for gzipped files to `fileio`.
    Last commit was actually more work to `PosteriorWorker` class
    in `workers` module.

--- pymsbayes/fileio.py
+++ pymsbayes/fileio.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from gzip import GzipFile
 from __builtin__ import file
 from __builtin__ import open as fopen
 
@@ -14,16 +15,30 @@ def expand_path(path):
     return os.path.abspath(os.path.expandvars(os.path.expanduser(path)))
 
 def open(*args, **kwargs):
+    if kwargs.has_key('name'):
+        kwargs['name'] = expand_path(kwargs['name'])
+        return FileStream(*args, **kwargs)
     path = expand_path(args[0])
     return FileStream(path, *args[1:], **kwargs)
 
-def process_file_arg(file_arg, mode='rU'):
+def process_file_arg(file_arg, mode='rU', compresslevel = None):
     close = False
     file_stream = file_arg
     if isinstance(file_arg, str):
-        file_stream = open(file_arg, mode)
+        if compresslevel:
+            file_stream = GzipFileStream(expand_path(file_arg), mode,
+                    compresslevel)
+        else:
+            file_stream = open(file_arg, mode)
         close = True
     return file_stream, close
+
+def is_gzip(file_arg):
+    fs, close = process_file_arg(file_arg)
+    l = fs.next()
+    if l.startswith("\x1f\x8b"):
+        return True
+    return False
 
 class FileStream(file):
     open_files = set()
@@ -34,3 +49,14 @@ class FileStream(file):
     def close(self):
         file.close(self)
         self.__class__.open_files.remove(self)
+
+class GzipFileStream(GzipFile):
+    open_files = set()
+    def __init__(self, *args, **kwargs):
+        GzipFile.__init__(self, *args, **kwargs)
+        self.__class__.open_files.add(self)
+
+    def close(self):
+        GzipFile.close(self)
+        self.__class__.open_files.remove(self)
+
