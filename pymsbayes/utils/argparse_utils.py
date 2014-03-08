2d25b962f0842c36361c08272f3db3764934e640
Jamie Oaks <joaks1@gmail.com>
2014-03-08 02:24:48 -0800

    Adding functions to `argparse_utils` module.
    Adding `arg_is_executable`, `arg_is_nonnegative_int`, and
    `arg_is_positive_float` functions for vetting CLI arguments to the
    `argparse_utils` module.

--- pymsbayes/utils/argparse_utils.py
+++ pymsbayes/utils/argparse_utils.py
@@ -5,7 +5,7 @@ import argparse
 
 from pymsbayes.fileio import expand_path
 from pymsbayes.config import MsBayesConfig
-from pymsbayes.utils.functions import is_file, is_dir
+from pymsbayes.utils.functions import is_file, is_dir, is_executable
 
 def arg_is_path(path):
     try:
@@ -42,4 +42,31 @@ def arg_is_dir(path):
         msg = '{0!r} is not a directory'.format(path)
         raise argparse.ArgumentTypeError(msg)
     return expand_path(path)
+
+def arg_is_executable(path):
+    try:
+        if not is_executable(path):
+            raise
+    except:
+        msg = '{0!r} is not an executable'.format(path)
+        raise argparse.ArgumentTypeError(msg)
+    return expand_path(path)
+
+def arg_is_nonnegative_int(i):
+    try:
+        if int(i) < 0:
+            raise
+    except:
+        msg = '{0!r} is not a non-negative integer'.format(i)
+        raise argparse.ArgumentTypeError(msg)
+    return int(i)
+
+def arg_is_positive_float(i):
+    try:
+        if float(i) <= 0.0:
+            raise
+    except:
+        msg = '{0!r} is not a positive real number'.format(i)
+        raise argparse.ArgumentTypeError(msg)
+    return float(i)
 
