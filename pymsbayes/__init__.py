ea1fbf6260d3507c8c45c53e82469960e082c4a4
Jamie Oaks <joaks1@gmail.com>
2014-04-08 00:38:20 -0700

    Updating module namespace.
    Adding package module imports to `pymsbayes.__init__.py`.

--- pymsbayes/__init__.py
+++ pymsbayes/__init__.py
@@ -22,3 +22,6 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
+from pymsbayes import teams
+from pymsbayes.utils import stats
+from pymsbayes import plotting
