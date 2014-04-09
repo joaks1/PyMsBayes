017e5c14d4c18dc1fddde36fc29b4f22aeb4d3c1
Jamie Oaks <joaks1@gmail.com>
2014-04-09 01:37:08 -0700

    Adding scripts.
    Adding `dmc_plot_results.py` and `dmc_prob_shared_divergence.py` scripts
    to the package's `scripts` directory.

---
+++ scripts/dmc_plot_results.py
@@ -0,0 +1,275 @@
+#! /usr/bin/env python
+
+"""
+CLI for generating plots of empirical results from dmc.py.
+"""
+
+import os
+import sys
+import random
+import multiprocessing
+import argparse
+
+from pymsbayes.utils import argparse_utils
+
+_program_info = {
+    'name': os.path.basename(__file__),
+    'author': 'Jamie Oaks',
+    'version': 'Version 0.1.0',
+    'description': __doc__,
+    'copyright': 'Copyright (C) 2013 Jamie Oaks',
+    'license': 'GNU GPL version 3 or later',}
+
+def main_cli():
+    description = '{name} {version}'.format(**_program_info)
+    parser = argparse.ArgumentParser(description = description)
+    parser.add_argument('info_path',
+            metavar='PYMSBAYES-INFO-FILE',
+            type=argparse_utils.arg_is_file,
+            help=('Path to `pymsbayes-info.txt` file.'))
+    parser.add_argument('-n', '--num-prior-samples',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            default = 100000,
+            help = ('The number of prior samples to simulate for estimating'
+                    'prior probabilities.'))
+    parser.add_argument('-i', '--iteration-index',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            help = ('The batch iteration index of results to be summarized. '
+                    'Output files should have a consistent schema. For '
+                    'example, a results file for divergence models might look '
+                    'something like '
+                    '`d1-m1-s1-151-div-model-results.txt`. In this example, '
+                    'the batch iteration index is "151". The default is to '
+                    'use the largest batch iteration index, which is probably '
+                    'what you want.'))
+    parser.add_argument('-o', '--output-dir',
+            action = 'store',
+            type = argparse_utils.arg_is_dir,
+            help = ('The directory in which all output plots will be written. '
+                    'The default is to use the directory of the pymsbayes info '
+                    'file.'))
+    parser.add_argument('--np',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            default = multiprocessing.cpu_count(),
+            help = ('The maximum number of processes to run in parallel. The '
+                    'default is the number of CPUs available on the machine.'))
+    parser.add_argument('--seed',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            help = 'Random number seed to use for the analysis.')
+    parser.add_argument('--version',
+            action = 'version',
+            version = '%(prog)s ' + _program_info['version'],
+            help = 'Report version and exit.')
+    parser.add_argument('--quiet',
+            action = 'store_true',
+            help = 'Run without verbose messaging.')
+    parser.add_argument('--debug',
+            action = 'store_true',
+            help = 'Run in debugging mode.')
+
+    args = parser.parse_args()
+
+    ##########################################################################
+    ## handle args
+
+    from pymsbayes.utils.messaging import (get_logger, LOGGING_LEVEL_ENV_VAR,
+            InfoLogger)
+
+    os.environ[LOGGING_LEVEL_ENV_VAR] = "INFO"
+    if args.quiet:
+        os.environ[LOGGING_LEVEL_ENV_VAR] = "WARNING"
+    if args.debug:
+        os.environ[LOGGING_LEVEL_ENV_VAR] = "DEBUG"
+    log = get_logger(__name__)
+
+    from pymsbayes import plotting
+    from pymsbayes.utils import parsing
+    from pymsbayes.utils import GLOBAL_RNG
+
+    if not plotting.MATPLOTLIB_AVAILABLE:
+        log.error(
+                '`matplotlib` could not be imported, so plots can not be\n'
+                'produced. Please install `matplotlib` and try again.')
+        sys.exit(1)
+
+    if not args.seed:
+        args.seed = random.randint(1, 999999999)
+    GLOBAL_RNG.seed(args.seed)
+
+    if not args.output_dir:
+        args.output_dir = os.path.dirname(args.info_path)
+    args.output_dir = os.path.join(args.output_dir, 'plots')
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+
+    results = parsing.DMCSimulationResults(args.info_path)
+    if results.num_sim_reps > 1:
+        log.error('Results appear to be from simulation-based analysis, '
+                'for which this plotting script is not appropriate.')
+        sys.exit(1)
+
+    observed_indices = sorted(results.observed_index_to_config.keys())
+    prior_indices = sorted(results.prior_index_to_config.keys())
+    for obs_idx in observed_indices:
+        for prior_idx in prior_indices:
+            result_indices = results.get_result_indices(obs_idx, prior_idx, 1)
+            result_idx = max(result_indices)
+            result_path_prefix = '{0}{1}-'.format(
+                    results.get_result_path_prefix(obs_idx, prior_idx, 1),
+                    result_idx)
+            result_dir = os.path.dirname(result_path_prefix)
+            out_prefix = os.path.join(args.output_dir, os.path.basename(
+                    result_path_prefix))
+            prior_cfg = results.prior_configs[prior_idx]
+            posterior_summary_path = get_result_path(result_path_prefix,
+                    'posterior_summary')
+            div_model_path = get_result_path(result_path_prefix,
+                    'div-model-results')
+            if results.sort_index == 0:
+                #plot marginal times
+                if not posterior_summary_path:
+                    log.warning('Could not find {0}{1}.txt(.gz); '
+                            'Skipping marginal times plot...'.format(
+                                    result_path_prefix,
+                                    'posterior-summary'))
+                else:
+                    marginal_times_plot = plotting.get_marginal_divergence_time_plot(
+                            config_path = results.prior_index_to_config[
+                                    prior_idx],
+                            posterior_summary_path = posterior_summary_path,
+                            labels = None,
+                            estimate = 'median',
+                            interval = 'HPD_95_interval',
+                            time_multiplier = 1.0,
+                            horizontal = True,
+                            label_dimension = 4.0,
+                            measure_dimension = 8.0,
+                            label_size = 12.0,
+                            measure_tick_label_size = 12.0,
+                            measure_axis_label = 'Divergence time',
+                            measure_axis_label_size = 14.0,
+                            label_axis_label = 'Taxon pair',
+                            label_axis_label_size = 14.0)
+                    marginal_times_path = '{0}{1}'.format(out_prefix,
+                            'marginal-divergence-times.pdf')
+                    margina_times_plot.savefig(marginal_times_path)
+
+                #plot top ordered models
+                if not div_model_path:
+                    log.warning('Could not find {0}{1}.txt(.gz); '
+                            'Skipping ordered div model plot...'.format(
+                                    result_path_prefix,
+                                    'div-model-results'))
+                else:
+                    div_model_plot = plotting.OrderedDivergenceModelPlotGrid(
+                            div_model_results_path = div_model_path,
+                            config_path = results.prior_index_to_config[
+                                    prior_idx],
+                            num_top_models = 10,
+                            height = 12.0,
+                            width = 8.0,
+                            plot_label_schema = 'uppercase',
+                            plot_label_offset = 0,
+                            plot_label_size = 12.0,
+                            y_title = 'Divergence time',
+                            y_title_size = 14.0,
+                            y_tick_label_size = 10.0,
+                            right_text_size = 10.0,
+                            margin_left = 0.03,
+                            margin_bottom = 0.0,
+                            margin_right = 1,
+                            margin_top = 0.99,
+                            padding_between_vertical = 0.8)
+                    plot = div_model_plot.create_grid()
+                    div_model_plot_path = '{0}{1}'.format(out_prefix,
+                            'ordered-div-models.pdf')
+                    plot.savefig(div_model_plot_path)
+
+            else:
+                #plot top unordered models
+                if not div_model_path:
+                    log.warning('Could not find {0}{1}.txt(.gz); '
+                            'Skipping unordered div model plot...'.format(
+                                    result_path_prefix,
+                                    'div-model-results'))
+                else:
+                    div_model_plot = plotting.UnorderedDivergenceModelPlotGrid(
+                            div_model_results_path = div_model_path,
+                            num_top_models = 10,
+                            height = 10.0,
+                            width = 8.0,
+                            data_label_size = 10.0,
+                            plot_label_schema = 'uppercase',
+                            plot_label_offset = 0,
+                            plot_label_size = 12.0,
+                            y_title = 'Divergence time',
+                            y_title_size = 14.0,
+                            y_tick_label_size = 10.0,
+                            right_text_size = 10.0,
+                            margin_left = 0.03,
+                            margin_bottom = 0.0,
+                            margin_right = 1,
+                            margin_top = 0.99,
+                            padding_between_vertical = 0.8)
+                    plot = div_model_plot.create_grid()
+                    div_model_plot_path = '{0}{1}'.format(out_prefix,
+                            'ordered-div-models.pdf')
+                    plot.savefig(div_model_plot_path)
+
+            #plot ndiv plot
+            psi_path = get_result_path(result_path_prefix,
+                    'psi-results')
+            if not psi_path:
+                log.warning('Could not find {0}{1}.txt(.gz); '
+                        'Skipping number of divergences plot...'.format(
+                                result_path_prefix,
+                                'psi-results'))
+            else:
+                num_div_summary = parsing.NumberOfDivergencesSummary(
+                        config_path = results.prior_index_to_config[prior_idx],
+                        psi_results_path = psi_path,
+                        posterior_summary_path = posterior_summary_path,
+                        num_prior_samples = args.num_prior_samples,
+                        num_processors = args.np)
+                num_div_summary.create_plot(
+                        plot_label_size = 10.0,
+                        right_text_size = 10.0,
+                        x_label_size = 10.0,
+                        y_label_size = 10.0,
+                        xtick_label_size = 10.0,
+                        ytick_label_size = 8.0,
+                        height = 6.0,
+                        width = 3.0,
+                        margin_bottom = 0.0,
+                        margin_left = 0.0,
+                        margin_top = 0.98,
+                        margin_right = 1.0,
+                        padding_between_vertical = 1.0)
+                num_div_plot_path = '{0}{1}'.format(out_prefix,
+                        'number-of-divergences.pdf')
+                num_div_summary.save_plot(num_div_plot_path)
+                
+                num_div_bf_path = '{0}{1}'.format(out_prefix,
+                        'number-of-divergences-bayes-factors.txt')
+                with open(num_div_bf_path, 'w') as out:
+                    out.write('num_of_divs\t2ln(bf)\n')
+                    for n in sorted(num_div_summary.psi_bayes_factors.keys()):
+                        out.write('{0}\t{1}\n'.format(n,
+                                num_div_summary.psi_bayes_factors[n]))
+
+def get_result_path(path_prefix, name):
+    p = '{0}{1}.txt'.format(path_prefix, name)
+    if os.path.exists(p):
+        return p
+    if os.path.exists(p + '.gz'):
+        return p + '.gz'
+    return None
+
+
+if __name__ == '__main__':
+    main_cli()
+
---
+++ scripts/dmc_prob_shared_divergence.py
@@ -0,0 +1,133 @@
+#! /usr/bin/env python
+
+"""
+CLI for estimating the probability of given taxon pairs co-diverging.
+"""
+
+import os
+import sys
+import math
+import random
+import multiprocessing
+import argparse
+
+from pymsbayes.utils import argparse_utils
+
+_program_info = {
+    'name': os.path.basename(__file__),
+    'author': 'Jamie Oaks',
+    'version': 'Version 0.1.0',
+    'description': __doc__,
+    'copyright': 'Copyright (C) 2013 Jamie Oaks',
+    'license': 'GNU GPL version 3 or later',}
+
+def main_cli():
+    description = '{name} {version}'.format(**_program_info)
+    parser = argparse.ArgumentParser(description = description)
+    parser.add_argument('div_model_path',
+            metavar='DIV-MODEL-RESULTS-FILE',
+            type=argparse_utils.arg_is_file,
+            help = ('Path to divergence model results file (i.e., '
+                    '`*-div-model-results.txt`).'))
+    parser.add_argument('-i', '--taxon-indices',
+            nargs = '+',
+            type = argparse_utils.arg_is_positive_int,
+            required = True,
+            help = ('Two or more space-separated indices of taxa for which to '
+                    'calculate the probability of them co-diverging. Indices '
+                    'correspond to the line in the sample table of the config, '
+                    'starting at 1 for the first line of the table. At least '
+                    'two indices are required.'))
+    parser.add_argument('-c', '--config',
+            type = argparse_utils.arg_is_config,
+            help = ('msBayes config file to be used to estimate prior '
+                    'probability via simulations. If provided, the '
+                    'posterior and prior probability and bayes factor is '
+                    'reported. If not provided, only the posterior '
+                    'probability is reported.'))
+    parser.add_argument('-n', '--num-prior-samples',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            default = 100000,
+            help = ('The number of prior samples to simulate for estimating'
+                    'prior probabilities. Only used if a config file is '
+                    'provided with the `-c` argument.'))
+    parser.add_argument('--np',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            default = multiprocessing.cpu_count(),
+            help = ('The maximum number of processes to run in parallel for '
+                    'prior simulations. The default is the number of CPUs '
+                    'available on the machine. This option is only relevant '
+                    'if a config file is provided using the `-c` argument.'))
+    parser.add_argument('--seed',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            help = ('Random number seed to use for simulations. This option '
+                    'is only relevant if a config file is provided using the '
+                    '`-c` argument.'))
+    parser.add_argument('--version',
+            action = 'version',
+            version = '%(prog)s ' + _program_info['version'],
+            help = 'Report version and exit.')
+    parser.add_argument('--quiet',
+            action = 'store_true',
+            help = 'Run without verbose messaging.')
+    parser.add_argument('--debug',
+            action = 'store_true',
+            help = 'Run in debugging mode.')
+
+    args = parser.parse_args()
+
+    ##########################################################################
+    ## handle args
+
+    from pymsbayes.utils.messaging import (get_logger, LOGGING_LEVEL_ENV_VAR,
+            InfoLogger)
+
+    os.environ[LOGGING_LEVEL_ENV_VAR] = "INFO"
+    if args.quiet:
+        os.environ[LOGGING_LEVEL_ENV_VAR] = "WARNING"
+    if args.debug:
+        os.environ[LOGGING_LEVEL_ENV_VAR] = "DEBUG"
+    log = get_logger(__name__)
+
+    from pymsbayes import config
+    from pymsbayes.teams import ModelProbabilityEstimatorTeam
+    from pymsbayes.utils import parsing, GLOBAL_RNG
+
+    if len(args.taxon_indices) < 2:
+        log.error('At least two taxon indices are required')
+        sys.exit(1)
+    if not args.seed:
+        args.seed = random.randint(1, 999999999)
+    GLOBAL_RNG.seed(args.seed)
+
+    div_models = parsing.OrderedDivergenceModelCollection(
+            div_model_results_path = args.div_model_path)
+    for i in args.taxon_indices:
+        if ((i < 1) or (i > div_models.npairs)):
+            log.error('taxon index {0} is out of bounds'.format(i))
+            sys.exit(1)
+    args.taxon_indices = [i - 1 for i in args.taxon_indices]
+    prob_shared_div = div_models.prob_of_shared_divergence(args.taxon_indices)
+
+    if args.config:
+        prob_estimator_team = ModelProbabilityEstimatorTeam(
+                config_paths = [args.config],
+                num_samples = args.num_prior_samples,
+                num_processors = args.np)
+        prob_estimator_team.start()
+        prior_prob = prob_estimator_team.shared_div_probs[args.config][
+                len(args.taxon_indices)]
+        bf = ((prob_shared_div / (1 - prob_shared_div)) /
+                (prior_prob / (1 - prior_prob)))
+
+    sys.stdout.write('posterior probability = {0}\n'.format(prob_shared_div))
+    if args.config:
+        sys.stdout.write('prior probability = {0}\n'.format(prior_prob))
+        sys.stdout.write('Bayes factor = {0}\n'.format(bf))
+        sys.stdout.write('2ln(Bayes factor) = {0}\n'.format(2 * math.log(bf)))
+if __name__ == '__main__':
+    main_cli()
+
