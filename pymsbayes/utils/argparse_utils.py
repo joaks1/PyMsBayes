b6b217b8b78963779fe374f9efa5b36fd8955c01
Jamie Oaks <joaks1@gmail.com>
2014-05-16 15:55:21 -0700

    Updating `argparse_utils` module.
    Adding `SmartHelpFormatter` class which overrides the
    `argparse.HelpFormatter._split_lines` method to allow customizable line breaks
    in the help string if the help message starts with "r|".
    
    Also adding `get_sort_index_help_message` function to return the (very long)
    help message for the sort index option.

--- pymsbayes/utils/argparse_utils.py
+++ pymsbayes/utils/argparse_utils.py
@@ -7,6 +7,17 @@ from pymsbayes.fileio import expand_path
 from pymsbayes.config import MsBayesConfig
 from pymsbayes.utils.functions import is_file, is_dir, is_executable
 
+class SmartHelpFormatter(argparse.HelpFormatter):
+    '''
+    A class to allow customizable line breaks for an argument help message
+    on a per argument basis.
+    '''
+
+    def _split_lines(self, text, width):
+        if text.startswith('r|'):
+            return text[2:].splitlines()
+        return argparse.HelpFormatter._split_lines(self, text, width)
+
 def arg_is_path(path):
     try:
         if not os.path.exists(path):
@@ -78,4 +89,45 @@ def arg_is_positive_float(i):
         msg = '{0!r} is not a positive real number'.format(i)
         raise argparse.ArgumentTypeError(msg)
     return float(i)
+
+def get_sort_index_help_message():
+    return (
+'''r|The sorting index used by
+`dpp-msbayes.pl`/`msbayes.pl` and `obsSumStats.pl`
+scripts to determine how the summary statistic vectors
+calculated from the alignments of the observed and
+simulated data are to be grouped and sorted.
+The default is %(default)s.
+0:    Do not group or sort. The identity and order of
+      the summary statistics of each alignment are
+      maintained and compared when calculating
+      Euclidean distance.
+1-7:  **NOTE**, options 1-7 all re-sort the summary
+      statistics in some way, and thus compare the
+      statistics from *different* alignments when
+      calculating the Euclidean distance.  This is not
+      valid and these options should *NOT* be used.
+      They are maintained for backwards compatibility
+      with the original msBayes.
+8-11: All of these options group the summary
+      statistics from multiple loci by taxon and then
+      calculate moments of each statistic across the
+      loci for each taxon, and then use these moments
+      to calculate Euclidean distance. The order of
+      the taxa is maintained, and so this is valid,
+      but you are losing a lot of information
+      contained in your loci by simply taking the mean
+      (option 11) across them. If you have A LOT of
+      loci, this sacrifice might be necessary to
+      reduce the number of summary statistics.
+      **NOTE**, options 8-10 are NOT well tested.
+      8:  Use the first 4 moments (mean, variance,
+          skewness, and kurtosis) of each statistic.
+      9:  Use the first 3 moments (mean, variance,
+          and skewness) of each statistic.
+      10: Use the first 2 moments (mean and variance)
+          of each statistic.
+      11: Use the first 1 moment (mean) of each
+          statistic.'''
+)
 
