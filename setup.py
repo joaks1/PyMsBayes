36b9308c1139b13130d4c800e801700673a73d3d
Jamie Oaks <joaks1@gmail.com>
2014-05-19 20:06:51 -0700

    Updating `setup.py`.
    Adding conditional install of bundled tools along with the package
    scripts. This is conditional on all of the tools passing as valid
    executables as determined by `subprocess.Popen`. If any tools fail this
    check, then just the scripts get installed.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 import sys, os
+import platform
+import subprocess
 
 from pymsbayes import __version__
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SCRIPTS_DIR = os.path.relpath(os.path.join(BASE_DIR, 'scripts'), BASE_DIR)
 SCRIPT_NAMES = [
         'dmc_dpp_summary.py',
         'dmc_plot_results.py',
@@ -15,7 +16,56 @@ SCRIPT_NAMES = [
         # 'dmc_prob_shared_divergence.py',
         'dmc_saturation_plot.py',
         ]
-SCRIPTS = [os.path.join(SCRIPTS_DIR, f) for f in SCRIPT_NAMES]
+TOOL_NAMES = [
+        'ABCestimator',
+        'dpp-msbayes.pl',
+        'dpp-msprior',
+        'eureject',
+        'msbayes.pl',
+        'msDQH',
+        'msprior',
+        'msReject',
+        'obsSumStats.pl',
+        'regress_cli.r',
+        'sumstatsvector',
+        ]
+
+def get_scripts_to_install():
+    script_dir = os.path.relpath(os.path.join(BASE_DIR, 'scripts'), BASE_DIR)
+    return [os.path.join(script_dir, f) for f in SCRIPT_NAMES]
+
+def check_tool(path):
+    try:
+        p = subprocess.Popen([path],
+                stdout = subprocess.PIPE,
+                stderr = subprocess.PIPE)
+        p.terminate()
+    except subprocess.CalledProcessError:
+        return path
+    except OSError:
+        return None
+    return path
+
+def get_tools_to_install():
+    platform_name = platform.system().lower()
+    # bin_dir = None
+    # if platform_name == 'linux':
+    #     bin_dir = os.path.join(BASE_DIR, "bin", "linux")
+    # elif platform_name == 'darwin':
+    bin_dir = os.path.join(BASE_DIR, "bin", "mac")
+    # elif platform_name == 'windows':
+    #     bin_dir = os.path.join(BASE_DIR, "bin", "win")
+    # else:
+    #     return []
+    tools = [os.path.join(bin_dir, f) for f in TOOL_NAMES]
+    ret = []
+    for t in tools:
+        if check_tool(t):
+            ret.append(t)
+        else:
+            return []
+    return ret
+
 
 setup(name='PyMsBayes',
       version=__version__,
@@ -29,7 +79,7 @@ Python msBayes wrapper""",
       url='',
       license="GPL",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      scripts=SCRIPTS,
+      scripts = get_scripts_to_install() + get_tools_to_install(),
       include_package_data=True,
       zip_safe=False,
       test_suite="pymsbayes.test.get_unittest_suite",
