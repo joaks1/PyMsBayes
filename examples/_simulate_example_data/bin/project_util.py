8e0a1efacdc64f737a893d000d56000d546719b9
Jamie Oaks <joaks1@gmail.com>
2014-08-13 01:31:09 -0700

    Working on code to simulate example data.

--- examples/_simulate_example_data/bin/project_util.py
+++ examples/_simulate_example_data/bin/project_util.py
@@ -5,6 +5,10 @@ import sys
 
 BIN_DIR = os.path.abspath(os.path.dirname(__file__))
 PROJECT_DIR = os.path.dirname(BIN_DIR)
+EXAMPLE_DIR = os.path.dirname(PROJECT_DIR)
+LIZARD_DIR = os.path.join(EXAMPLE_DIR, 'lizards')
+LIZARD_SEQ_DIR = os.path.join(LIZARD_DIR, 'sequences')
+LIZARD_CONFIG_DIR = os.path.join(LIZARD_DIR, 'configs')
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
 SP_TREE_DIR = os.path.join(DATA_DIR, 'species-tree')
 SP_TREE_PATH = os.path.join(SP_TREE_DIR, 'species-tree.nex')
--- examples/_simulate_example_data/bin/simulate-data.py
+++ examples/_simulate_example_data/bin/simulate-data.py
@@ -2,14 +2,131 @@
 
 import os
 import sys
+import re
+import subprocess
 import multiprocessing
 
-from pymsbayes.utils import messaging
+from seqsift.utils import dataio
+from seqsift.seqops import seqstats
+
+from pymsbayes.utils import messaging, ToolPathManager, tempfs, functions, stats
 from pymsbayes import workers
 from pymsbayes import manager
 import project_util
 
 _LOG = messaging.get_logger(__name__)
+
+number_pattern_string = r'[\d\.Ee\-\+]+'
+paup_hky_string = (
+        '^\s*Estimated\s+base\s+frequencies\s+=\s+'
+        'A:(?P<a>{0})\s+C:(?P<c>{0})\s+'
+        'G:(?P<g>{0})\s+T:(?P<t>{0})'
+        '\n\s*Estimated\s+ti/tv\s+ratio\s+=\s+{0}\s*'
+        '\(kappa\s+=\s+(?P<kappa>{0})\)\s*$'.format(number_pattern_string)
+        )
+paup_hky_pattern = re.compile(paup_hky_string, re.MULTILINE)
+
+def write_paup_hky_file(path, nexus_data_path, score_file_path):
+    with open(path, 'w') as out:
+        out.write("#NEXUS\n\n"
+                "BEGIN PAUP;\n\t"
+                "set warnreset=no; set increase=auto; set warnroot=no;\n\t"
+                "execute {0};\n\t"
+                "DSet distance=LOGDET objective=ME base=equal rates=equal pinv=0\n\t"
+                "subst=all negbrlen=setzero;\n\t"
+                "NJ showtree=no breakties=random;\n"
+                "END;\n\n"
+                "BEGIN PAUP;\n\t"
+                "Default lscores longfmt=yes;\n\t"
+                "Set criterion=like;\n\t"
+                "[!** Calculating HKY **]\n\t"
+                "lscores 1/ nst=2 base=est tratio=est rates=equal pinv=0\n\t"
+                "scorefile={1} append;\n"
+                "END;\n".format(nexus_data_path, score_file_path)
+                )
+
+def parse_paup_log_file(path):
+    with open(path, 'r') as in_stream:
+        log = in_stream.read()
+        match_iter = paup_hky_pattern.finditer(log)
+        i = -1
+        for i, m in enumerate(match_iter):
+            pass
+        if i < 0:
+            return {'kappa': 1.0,
+                    'a': 0.25,
+                    'c': 0.25,
+                    'g': 0.25,
+                    't': 0.25}
+        elif i > 0:
+            raise Exception('found multiple sets of HKY parameters in '
+                    '{0!r}'.format(path))
+    return m.groupdict()
+
+class PaupWorker(object):
+    count = 0
+    def __init__(self,
+            nex_path,
+            exe_path = None,
+            stdout_path = None,
+            stderr_path = None,
+            tag = None):
+        self.stdout_path = stdout_path
+        self.stderr_path = stderr_path
+        self.tag = tag
+        self.__class__.count += 1
+        self.name = self.__class__.__name__ + '-' + str(self.count)
+        if not exe_path:
+            exe_path = 'paup'
+        self.exe_path = ToolPathManager.get_external_tool(exe_path)
+        self.nex_path = nex_path
+        self.cmd = [self.exe_path, '-n', self.nex_path]
+
+    def get_stderr(self):
+        if not self.stderr_path:
+            return self.stderr
+        try:
+            se = open(self.stderr_path, 'r')
+        except IOError, e:
+            _LOG.error('Could not open stderr file')
+            raise e
+        msg = se.read()
+        se.close()
+        return msg
+
+    def start(self):
+        _LOG.debug('Starting process with following command:\n\t'
+                '{0}'.format(' '.join(self.cmd)))
+        if self.stdout_path:
+            sout = open(self.stdout_path, 'w')
+        else:
+            sout = subprocess.PIPE
+        if self.stderr_path:
+            serr = open(self.stderr_path, 'w')
+        else:
+            serr = subprocess.PIPE
+        self.process = subprocess.Popen(self.cmd,
+                stdout = sout,
+                stderr = serr,
+                shell = False)
+        self.stdout, self.stderr = self.process.communicate()
+        self.exit_code = self.process.wait()
+        if hasattr(sout, 'close'):
+            sout.close()
+        if hasattr(serr, 'close'):
+            serr.close()
+        # if self.exit_code != 0:
+        #         _LOG.error('execution failed')
+        #         raise WorkerExecutionError('{0} failed.\ninvocation:\n{1}\n'
+        #                 'stderr:\n{2}'.format(
+        #                 self.name, ' '.join([str(x) for x in self.cmd]),
+        #                 self.get_stderr()))
+        self.finished = True
+
+def check_for_output(path):
+    if os.path.exists(path):
+        raise Exception('script has already been run: output files already '
+                'exist... aborting')
 
 def simulate_alignments(
         num_individuals_per_pop = 5,
@@ -22,6 +139,7 @@ def simulate_alignments(
         os.mkdir(project_util.SEQ_DIR)
     tree_workers = []
     seq_workers = []
+    seq_file_paths = []
     for i, align_len in enumerate(alignment_lengths):
         tree_path = os.path.join(project_util.GENE_TREE_DIR,
                 'gene-tree-locus-{0}.nex'.format(i))
@@ -39,9 +157,7 @@ def simulate_alignments(
             # (all branches of species tree are multiplied by 4) so that the
             # difference in ploidy cancels out. E.g., 2Nu = 0.5N4u
             sp_tree_path = project_util.SP_TREE_MT_PATH
-        if os.path.exists(tree_path):
-            raise Exception('script has already been run: output files already '
-                    'exist... aborting')
+        check_for_output(tree_path)
         tw = workers.GeneTreeSimWorker(args = [
                 '-n', '1',
                 '-t', num_individuals_per_pop * num_locus_copies,
@@ -54,14 +170,105 @@ def simulate_alignments(
                 output_path = seq_path)
         tree_workers.append(tw)
         seq_workers.append(sw)
+        seq_file_paths.append(seq_path)
     tree_workers = manager.Manager.run_workers(tree_workers,
             num_processors = multiprocessing.cpu_count())
     seq_workers = manager.Manager.run_workers(seq_workers,
             num_processors = multiprocessing.cpu_count())
-    return 
+    return seq_file_paths
+
+
+def parse_alignments(paths):
+    temp_fs = tempfs.TempFileSystem(
+            parent = project_util.BIN_DIR)
+    if not os.path.exists(project_util.LIZARD_SEQ_DIR):
+        functions.mkdr(project_util.LIZARD_SEQ_DIR)
+    if not os.path.exists(project_util.LIZARD_CONFIG_DIR):
+        functions.mkdr(project_util.LIZARD_CONFIG_DIR)
+    config_path = os.path.join(project_util.LIZARD_CONFIG_DIR, 'sample-table.txt')
+    check_for_output(config_path)
+    config_stream = open(config_path, 'w')
+    pi_path = os.path.join(project_util.LIZARD_CONFIG_DIR, 'theta-estimates.txt')
+    check_for_output(pi_path)
+    pi_stream = open(pi_path, 'w')
+    pi_stream.write('species\tpopulation\tlocus\tpi\n')
+    pi_summary = stats.SampleSummarizer()
+    id_pattern = re.compile(
+            r'^species(?P<species>\d)_pop(?P<pop>\d)_tip(?P<tip>\d)$')
+    data_sets = {}
+    for p in paths:
+        locus = os.path.splitext(p)[0].split('-')[-1]
+        sequences = dataio.get_seq_dict(p, format = 'nexus')
+        for seq_id, seq in sequences.iteritems():
+            m = id_pattern.match(seq_id)
+            sp = m.group('species')
+            pop = m.group('pop')
+            if not sp in data_sets:
+                data_sets[sp] = {}
+            if not pop in data_sets[sp]:
+                data_sets[sp][pop] = {}
+            data_sets[sp][pop][seq_id] = seq
+        for species in sorted(data_sets.keys()):
+            pop_dict = data_sets[species]
+            seq_list = []
+            parameters = {}
+            for population, seq_dict in pop_dict.iteritems():
+                seq_ids = sorted(seq_dict.keys())
+                seq_list.extend((seq_dict[k] for k in seq_ids))
+                pi = seqstats.average_number_of_pairwise_differences(
+                    seq_iter = (seq_dict[k] for k in seq_ids),
+                    aligned = True,
+                    per_site = True)
+                pi_summary.add_sample(pi)
+                pi_stream.write('\t{0}\t{1}\t{2}\t{3}\n'.format(species,
+                        population, locus, pi))
+            fasta_path = os.path.join(project_util.LIZARD_SEQ_DIR,
+                    'species-{0}-locus-{1}.fasta'.format(species, locus))
+            check_for_output(fasta_path)
+            dataio.write_seqs(seq_list,
+                    dest = fasta_path,
+                    format = 'fasta')
+            tmp_nex_data_path = temp_fs.get_file_path()
+            tmp_nex_exe_path = temp_fs.get_file_path()
+            tmp_score_path = temp_fs.get_file_path()
+            nseqs = dataio.convert_format(in_file = fasta_path,
+                    out_file = tmp_nex_data_path,
+                    in_format = 'fasta',
+                    out_format = 'nexus')
+            write_paup_hky_file(tmp_nex_exe_path, tmp_nex_data_path,
+                    tmp_score_path)
+            stdout_path = temp_fs.get_file_path()
+            pw = PaupWorker(nex_path = tmp_nex_exe_path,
+                    stdout_path = stdout_path)
+            pw.start()
+            parameters = parse_paup_log_file(stdout_path)
+            parameters['species'] = 'species-{0}'.format(species)
+            parameters['locus'] = 'locus-{0}'.format(locus)
+            if locus.lower() == 'mt':
+                parameters['ploidy_multiplier'] = 0.25
+                parameters['rate_multiplier'] = 4.0
+            else:
+                parameters['ploidy_multiplier'] = 1.0
+                parameters['rate_multiplier'] = 1.0
+            parameters['nsamples1'] = len(pop_dict['1'])
+            parameters['nsamples2'] = len(pop_dict['2'])
+            assert parameters['nsamples1'] + parameters['nsamples2'] == nseqs
+            parameters['nsites'] = len(pop_dict['1'].values()[0].seq)
+            parameters['path'] = os.path.relpath(fasta_path,
+                    os.path.dirname(config_path))
+            if (parameters['kappa'] < 1.0) or (parameters['kappa'] > 1000.0):
+                parameters['kappa'] = 1.0
+            config_stream.write('{species}\t{locus}\t{ploidy_multiplier}\t'
+                    '{rate_multiplier}\t{nsamples1}\t{nsamples2}\t{kappa}\t'
+                    '{nsites}\t{a}\t{c}\t{g}\t{path}\n'.format(**parameters))
+    sys.stdout.write('Summary of pi estimates:\n{0}\n'.format(str(pi_summary)))
+    config_stream.close()
+    pi_stream.close()
+    temp_fs.purge()
 
 def main():
-    simulate_alignments()
+    paths = simulate_alignments()
+    parse_alignments(paths)
 
 
 if __name__ == '__main__':
