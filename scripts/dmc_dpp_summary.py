8fa66dfbbefd1f858b1f5f821f23ce5f8a47ffb7
Jamie Oaks <joaks1@gmail.com>
2015-01-09 21:19:32 -0800

    Completing extension of ``dmc_dpp_summary.py``.
    New options to the ``dmc_dpp_summary.py`` script to allow the estimation
    of the probabilities of the number of categories is now up and running.

--- scripts/dmc_dpp_summary.py
+++ scripts/dmc_dpp_summary.py
@@ -7,6 +7,8 @@ Dirichlet process calculations.
 import os
 import sys
 import argparse
+import random
+import multiprocessing
 
 from pymsbayes.utils import argparse_utils
 
@@ -64,21 +66,39 @@ def main():
             action = 'store',
             type = int,
             required = False,
-            help = ('The number of replicates to simulate for '
-                    'proabability estimates.'))
-    parser.add_argument('npairs',
+            help = ('The number of simulation replicates to use for\n'
+                    'estimating the probability of the number of categories.\n'
+                    'By default, no simulations are run and the probabilities\n'
+                    'are not estimated or reported.'))
+    parser.add_argument('--np',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            default = multiprocessing.cpu_count(),
+            help = ('The maximum number of processes to run in parallel. The\n'
+                    'default is the number of CPUs available on the machine.\n'
+                    'This option is only used if `--reps` is specified.'))
+    parser.add_argument('--seed',
+            action = 'store',
+            type = argparse_utils.arg_is_positive_int,
+            help = 'Random number seed to use for the analysis.')
+    parser.add_argument('num_elements',
             metavar='N',
             type=argparse_utils.arg_is_nonnegative_int,
             help='Number of elements (i.e., number of taxon pairs).')
 
     args = parser.parse_args()
 
+    from pymsbayes.utils.messaging import (LoggingControl,
+            InfoLogger)
+
+    LoggingControl.set_logging_level("WARNING")
+
 
     from pymsbayes.teams import DppSimTeam
     from pymsbayes.utils import GLOBAL_RNG, probability
     from pymsbayes.utils.stats import Partition
 
-    p = Partition('0' * args.npairs)
+    p = Partition('0' * args.num_elements)
 
     results = dict(zip(parameter_options,
             [None for k in parameter_options]))
@@ -88,6 +108,13 @@ def main():
         results['ncats'] = p.get_dpp_expected_num_cats(args.parameter_value)
 
     elif args.parameter == 'ncats':
+        if args.parameter_value > args.num_elements:
+            sys.stderr.write('ERROR: `ncats` cannot be greater than the number '
+                    'of elements\n')
+            sys.exit(1)
+        elif args.parameter_value < 1.0:
+            sys.stderr.write('ERROR: `ncats` cannot be less than 1\n')
+            sys.exit(1)
         results['ncats'] = args.parameter_value
         results['concentration'] = p.get_dpp_concentration(args.parameter_value)
 
@@ -104,30 +131,33 @@ def main():
                 shape = results['shape'],
                 scale = results['scale'])
         
-    sys.stdout.write('number of elements = {0}\n'.format(args.npairs))
+    sys.stdout.write('number of elements = {0}\n'.format(args.num_elements))
     for key in parameter_options:
         sys.stdout.write('{0} = {1}\n'.format(
                 keys_to_print.get(key, key),
                 results[key]))
 
     if args.reps:
+        sys.stderr.write('\nStarting simulations to estimate probabilities...\n')
         if not args.seed:
             args.seed = random.randint(1, 999999999)
-        log.info('Using seed {0}'.format(args.seed))
+        sys.stderr.write('Using seed {0}\n\n'.format(args.seed))
         GLOBAL_RNG.seed(args.seed)
 
         sim_team = DppSimTeam(
                 alpha = alpha,
-                num_elements = args.npairs,
+                num_elements = args.num_elements,
                 base_distribution = None,
-                num_samples = args.num_prior_samples,
+                num_samples = args.reps,
                 num_processors = args.np)
         sim_team.start()
 
-        for k, p in sim_team.psi_probs.iteritems():
-            sys.stdout.write('\tnum of divergence events = {0}: {1}\n'.format(
+        sys.stderr.write('Estimated probabilities of the number of categories:\n')
+        for k, prob in sim_team.psi_probs.iteritems():
+            sys.stdout.write('\tp(ncats = {0}) = {1:.4f} (n = {2})\n'.format(
                     k,
-                    p))
+                    prob,
+                    p.number_of_partitions_into_k_subsets(k)))
 
 if __name__ == '__main__':
     main()
