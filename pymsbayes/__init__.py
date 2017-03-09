54f720df9da14ed40e6d9632401f59c6dcb8eaa5
Jamie Oaks <joaks1@gmail.com>
2017-03-08 21:59:58 -0600

    Incrementing version to 0.3.7.

--- pymsbayes/__init__.py
+++ pymsbayes/__init__.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 __project__ = "PyMsBayes"
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 __author__ = "Jamie Oaks"
 __copyright__ = "Copyright 2013 Jamie Oaks."
 __license__ = """
